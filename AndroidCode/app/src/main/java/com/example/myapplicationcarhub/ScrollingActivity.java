0e5aec4cea920ae97048f8a4b8e063721978644c
mohelt <moheltbusiness@gmail.com>
2021-09-10 18:42:26 +0100

    Added email button functionalitya

--- AndroidCode/app/src/main/java/com/example/myapplicationcarhub/ScrollingActivity.java
+++ AndroidCode/app/src/main/java/com/example/myapplicationcarhub/ScrollingActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplicationcarhub;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -28,8 +30,10 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri data = Uri.parse("mailto:moheltbusiness@gmail.com?subject=Query:");
+                intent.setData(data);
+                startActivity(intent);
             }
         });
     }
